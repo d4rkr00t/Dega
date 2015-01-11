edac3ccc8a79f77df2303e14fc7e658ed0aaa0a4
Stanislav Sysoev <d4rkr00t@gmail.com>
2015-01-11 22:23:47 +0300

    Simple dega features

--- dega.js
+++ dega.js
@@ -5,7 +5,6 @@
  *
  * @description
  * Event delegation library on plain JavaScript
- *
  */
 
 (function(root, factory) {
@@ -19,8 +18,49 @@
 })(this, function() {
     var _instances = [];
 
-    function _findElem(selector) {
+    function _matchElem(el, selector) {
+        if (selector.match(/^\./)) {
+            return el.classList.contains(selector.replace(/^\./, ''));
+        } else if (selector.match(/^#/)) {
+            return el.id === selector.replace(/^#/, '');
+        } else {
+            return el.tagName === selector.toUpperCase();
+        }
+    }
 
+    function _findEventListener(eventListeners, callback, useCapture) {
+        for (var i = 0; i < eventListeners.length; i++) {
+            if (eventListeners[i].original === callback && eventListeners[i].useCapture === useCapture) {
+                return eventListeners[i];
+            }
+        }
+
+        return false;
+    }
+
+    function _generateUseCapture(type, useCapture) {
+        return type === 'blur' || type === 'focus' || useCapture;
+    }
+
+    function _generateEventListener(selector, callback, useCapture) {
+        return {
+            original: callback,
+            useCapture: useCapture ,
+            generated: function(e) {
+                var el = e.target;
+
+                while (true) {
+                    if (_matchElem(el, selector)) {
+                        callback.call(el, e);
+                        break;
+                    } else {
+                        el = el.parentNode;
+                    }
+
+                    if (!el) { break; }
+                }
+            }
+        };
     }
 
     var Dega = function(elem) {
@@ -37,18 +77,29 @@
         }
 
         if (typeof elem === 'String') {
-            elem = _findElem(elem);
+            elem = document.querySelector(elem);
         }
 
         this.elem = elem;
+        this._eventListeners = {};
     };
 
-    Dega.prototype.on = function(event, selector, callback, useCapture) {
+    Dega.prototype.on = function(type, selector, callback, useCapture) {
+        useCapture = _generateUseCapture(type, useCapture);
 
+        var eventListener = _generateEventListener(selector, callback, useCapture);
+
+        this._eventListeners.push(eventListener);
+        this.elem.addEventListener(type, eventListener.generatedCallback, eventListener.useCapture);
     };
 
-    Dega.prototype.off = function(event, selector, callback) {
+    Dega.prototype.off = function(type, selector, callback, useCapture) {
+        useCapture = _generateUseCapture(type, useCapture);
 
+        var eventListener = _findEventListener(this._eventListeners, callback, useCapture);
+
+        if (eventListener) {
+            this.elem.removeEventListener(type, eventListener.generatedCallback, eventListener.useCapture);
+        }
     };
-
 });
